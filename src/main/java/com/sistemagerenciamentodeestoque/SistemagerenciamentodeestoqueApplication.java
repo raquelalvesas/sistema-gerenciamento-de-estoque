2b58db7e99176f3146dbc4c504b08247ae8b11f1
raquel <raquelalvesas@gmail.com>
2023-07-01 18:13:57 -0300

    projeto inicial

---
+++ src/main/java/com/sistemagerenciamentodeestoque/SistemagerenciamentodeestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.sistemagerenciamentodeestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemagerenciamentodeestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemagerenciamentodeestoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemagerenciamentodeestoque/SistemagerenciamentodeestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemagerenciamentodeestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemagerenciamentodeestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
