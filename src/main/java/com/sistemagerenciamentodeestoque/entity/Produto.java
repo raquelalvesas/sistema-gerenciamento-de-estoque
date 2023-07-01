b109e43fa5fa9913bfa22fdae65538ac1fd21613
raquel <raquelalvesas@gmail.com>
2023-07-01 19:58:11 -0300

    feat: inclusao produto

---
+++ src/main/java/com/sistemagerenciamentodeestoque/entity/Produto.java
@@ -0,0 +1,32 @@
+package com.sistemagerenciamentodeestoque.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "produto")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Produto extends Auditavel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "descricao")
+    private String descricao;
+    @Column(name = "valor_unitario")
+    private Double valorUnitario;
+    @Column(name = "peso_unitario")
+    private String pesoUnitario;
+    @Column(name = "quantidade_estoque")
+    private Long quantidadeEstoque;
+    @Column(name = "status")
+    private String status;
+    @Column(name = "data_vencimento")
+    private Date dataVencimento;
+
+}
